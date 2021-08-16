cfc0605b7e8a305ed2ff047922cc149de30558a7
yoyogold-a11 <tmrfack@gmail.com>
2021-08-16 02:33:35 -0500

    testing

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,13 @@
-# conf file
+# Turns off pass and declare identity file
 
-fie { 'Conf file':
+file_line { 'Turn off passwd auth':
   ensure => 'present',
-  name   => 'ssh_config',
-  path   => '/etc/ssh/ssh_config'
+  path   => '/etc/ssh/ssh_config',
+  line   => '  PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '  IdentityFile ~/.ssh/holberton'
-}
-
-file_line { 'Turn off passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '  PasswordAuthentication no'
+  line   => '  IdentityFile ~/.ssh/holberton',
 }
