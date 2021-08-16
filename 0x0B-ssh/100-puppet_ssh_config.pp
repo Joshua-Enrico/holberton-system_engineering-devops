bf13c89d4ff7bdafd4e7f6bf84358e3f53efb262
yoyogold-a11 <tmrfack@gmail.com>
2021-08-16 02:22:57 -0500

    fixing

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,18 @@
 # conf file
 
-fie { 'conf file':
+fie { 'Conf file':
   ensure => 'present',
   name   => 'ssh_config',
   path   => '/etc/ssh/ssh_config'
 }
 
-file_line { 'idendity file':
+file_line { 'Idendity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '  IdentityFile ~/.ssh/holberton'
 }
 
-file_line { 'turn of authentication':
+file_line { 'Turn of authentication':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '  PasswordAuthentication no'
