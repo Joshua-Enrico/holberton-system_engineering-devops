703cd7d3761874e7dbfe5db6fcd561995d8c0cda
yoyogold-a11 <tmrfack@gmail.com>
2021-09-13 16:53:51 -0500

    several adittions

--- 0x14-mysql/script-used/replica_user.sql
+++ 0x14-mysql/script-used/replica_user.sql
@@ -1,7 +1,5 @@
--- Write a script that creates the MySQL server user user_0d_1.
---      user_0d_1 should have all privileges on your MySQL server
---      The user_0d_1 password should be set to user_0d_1_pwd
---      If the user user_0d_1 already exists, your script should not fail
+-- Write a script that creates the MySQL server user 
+
 
 CREATE USER IF NOT EXISTS 'replica_user'@'%' IDENTIFIED BY '1';
-GRANT ALL PRIVILEGES ON *.* TO 'replica_user'@'%';
+GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
