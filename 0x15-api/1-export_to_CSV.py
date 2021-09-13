699b6effbf872f8dba898948773a9ea504f11e92
yoyogold-a11 <tmrfack@gmail.com>
2021-09-13 17:03:46 -0500

    fixing

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-"""
-Uses https://jsonplaceholder.typicode.com
-"""
-
+"""Uses https://jsonplaceholder.typicode.com"""
 import csv
 import requests
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    userId = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(userId), verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(userId), verify=False).json()
-    with open("{}.csv".format(userId), 'w', newline='') as csvfile:
-        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in todo:
-            taskwriter.writerow([int(userId), user.get('username'),
-                                 task.get('completed'),
-                                 task.get('title')])
+
+if __name__ == "__main__":
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                     .format(sys.argv[1]))
+    d = r.json()
+    name = d.get('username')
+    user_id = d.get('id')
+    r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    d_todos = r.json()
+    titles = []
+    completed = []
+    for i in d_todos:
+        if i.get('userId') == user_id:
+            titles.append(i.get('title'))
+            completed.append(i.get('completed'))
+    with open('{}.csv'.format(sys.argv[1]), mode='w') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL)
+        for i, e in enumerate(titles):
+            writer.writerow([user_id, name, completed[i], e])
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
-"""
-Uses https://jsonplaceholder.typicode.com
-"""
-
+"""Uses https://jsonplaceholder.typicode.com"""
 import json
 import requests
 
-if __name__ == '__main__':
-    users = requests.get("https://jsonplaceholder.typicode.com/users",
-                         verify=False).json()
-    userdict = {}
-    usernamedict = {}
-    for user in users:
-        uid = user.get("id")
-        userdict[uid] = []
-        usernamedict[uid] = user.get("username")
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos",
-                        verify=False).json()
-    for task in todo:
-        taskdict = {}
-        uid = task.get("userId")
-        taskdict["task"] = task.get('title')
-        taskdict["completed"] = task.get('completed')
-        taskdict["username"] = usernamedict.get(uid)
-        userdict.get(uid).append(taskdict)
-    with open("todo_all_employees.json", 'w') as jsonfile:
-        json.dump(userdict, jsonfile)
+
+if __name__ == "__main__":
+    r = requests.get('https://jsonplaceholder.typicode.com/users')
+    users = r.json()
+    r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    tasks = r.json()
+    dic = {str(d.get('id')): [{'username': d.get('username'),
+                               'task': i.get('title'), 'completed':
+                               i.get('completed')}
+                              for i in tasks
+                              if i.get('userId') == d.get('id')]
+           for d in users}
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(dic, json_file)
